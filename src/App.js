9c329d223b86ea97791c0da9a0ffb6bf67524cc6
vimukthi <vimukthi@efuturesworld.com>
2019-09-25 01:43:45 +0530

    get idea about coonditional

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import './App.css';
 class App extends Component{
 
   render(){
+      const name="john Doe";
+      const loading =true;
+      const showName=false;//&& use if no else condition
       return (
           <div className="App">
-              <h1>hello from react</h1>
+              {loading ? <h1>loading ..</h1> :<h1>hello {showName && name}</h1> }
+
           </div>
       );
   }
