93a6fb3b5dd4690a7a0115b2ec88ef341d17278d
vimukthi <vimukthi@efuturesworld.com>
2019-09-25 01:48:14 +0530

    remove unwanted

--- src/App.js
+++ src/App.js
@@ -4,13 +4,10 @@ import './App.css';
 class App extends Component{
 
   render(){
-      const name="john Doe";
-      const loading =true;
-      const showName=false;//&& use if no else condition
+
       return (
           <div className="App">
-              {loading ? <h1>loading ..</h1> :<h1>hello {showName && name}</h1> }
-
+              <h1>hello from react</h1>
           </div>
       );
   }
