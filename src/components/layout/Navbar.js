7e7e2f08461f14d9cddc35a18c4f7a94cec00ef5
vimukthi <vimukthi@efuturesworld.com>
2019-10-13 23:19:59 +0530

    refromat code in NavBar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
-const Navbar =(props)=>{
+const Navbar = (props) => {
 
-        return (
-            <nav  className="navbar bg-primary">
-                <h1>
-                    <i className={props.icon}/> {props.title}
-                </h1>
-            </nav>
-        );
+    return (
+        <nav className="navbar bg-primary">
+            <h1>
+                <i className={props.icon}/> {props.title}
+            </h1>
+        </nav>
+    );
 
 }
 
-Navbar.defaultProps ={
-    title:"github finder",
-    icon:"fa fa-github"
+Navbar.defaultProps = {
+    title: "github finder",
+    icon: "fa fa-github"
 }
 
 Navbar.propTypes = {
-    title:PropTypes.string.isRequired,
-    icon:PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
 }
 
 export default Navbar;
