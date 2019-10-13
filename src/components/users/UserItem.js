47b2213dc58c14fab07dade0f6c46e96fd7eff42
vimukthi <vimukthi@efuturesworld.com>
2019-10-13 23:15:38 +0530

    convert UserItem class to function

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,17 +1,8 @@
-import React,{Component} from 'react';
+import React from 'react';
 
-class UserItem extends Component{
+const UserItem =(props)=>{
 
-    state={
-            id:"id",
-            login:"mojombo",
-            avatar_url:"https://avatars0.githubusercontent.com/u/1?v=4",
-            html_url:"https://github.com/mojombo"
-        }
-
-
-    render(){
-        const {login,avatar_url,html_url} = this.props.user;
+        const {login,avatar_url,html_url} = props.user;
 
         return (
             <div  className="card text-center">
@@ -27,7 +18,6 @@ class UserItem extends Component{
     </div>
             </div>
         );
-    }
 
 }
 
